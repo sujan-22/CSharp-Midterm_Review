718c2aa479f2bac5ad934814f36e3b67190ebbe8
Sujan <120612188+sujan-22@users.noreply.github.com>
2023-10-25 11:53:06 -0400

    Update Program.cs

--- Midterm_review/Program.cs
+++ Midterm_review/Program.cs
@@ -1,58 +1,79 @@
-﻿using System;
+using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Midterm_review
+namespace Ex
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            List<Fruit> myFruits  = new List<Fruit>();
-            List<Electronic> myElectronic  = new List<Electronic>();
+            List<Fruit> fruits = new List<Fruit>();
+            List<Electronic> electronics = new List<Electronic>();
+            
 
-            Fruit first = new Fruit(20, "Apple", "Red Apple", 10);
-            myFruits.Add(first);
+            String[] parts;
 
-            Fruit second = new Fruit(50, "Lemon", "Yellow Lemon", 6);
-            myFruits.Add(second);
-
-            Fruit third = new Fruit(51, "Grape", "Green Grape", 8);
-            myFruits.Add(third);
-
-            Fruit fourth = new Fruit(96, "Pear", "Brown Pear", 80);
-            myFruits.Add(fourth);
-
-            //
-
-            Electronic first1 = new Electronic(30, "IPhone", "IPhone X",30, 10);
-            myElectronic.Add(first1);
-
-            Electronic second2 = new Electronic(40, "TV", "85 TV",5,200);
-            myElectronic.Add(second2);
-
-            Electronic third3 = new Electronic(80, "Laptop", "Thinkpad",9,50);
-            myElectronic.Add(third3);
-
-            Electronic fourth4 = new Electronic(82, "Keyboard", "Keys",2,5);
-            myElectronic.Add(fourth4);
-
-            int totalDays = 0;
-            foreach(var item in myFruits)
+            try
             {
-               
-                Console.WriteLine(item.ToString());
-                totalDays += item.GetTotalDeliveryDays();
+                using (StreamReader sr = new StreamReader("cs.txt"))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        parts = line.Split(',');
+                        
+
+                        if (parts.Length == 4 ) {
+                            int sku = int.Parse(parts[0].Trim());
+                            string name = parts[1].Trim();
+                            string desc = parts[2].Trim();
+                            int quantity = int.Parse(parts[3].Trim());
+
+                            Fruit fruit = new Fruit(sku, name, desc, quantity);
+                            fruits.Add(fruit);
+                        } else
+                        {
+                            int sku = int.Parse(parts[0].Trim());
+                            string name = parts[1].Trim();
+                            string desc = parts[2].Trim();
+                            int quantity = int.Parse(parts[3].Trim());
+                            int weight = int.Parse(parts[4].Trim());   
+                            Electronic electronic = new Electronic(sku, name, desc, quantity, weight);
+                            electronics.Add(electronic);
+                        }
+                    }
+                    
+                }
+
+                foreach(var  f in fruits)
+                {
+                    Console.WriteLine(f.ToString());
+                }
+                foreach (var item in electronics)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+
+
+                
+
             }
-            foreach (var item in myElectronic)
+            catch (Exception ex)
             {
-                Console.WriteLine(item.ToString());
-                totalDays += item.GetTotalDeliveryDays();
+                Console.WriteLine("Could not read file");
+                Console.WriteLine(ex.Message);
             }
-            Console.WriteLine($"total days: {totalDays}");
             Console.ReadLine();
+
+
+           
+
         }
-    }
+        
+    } 
 }
